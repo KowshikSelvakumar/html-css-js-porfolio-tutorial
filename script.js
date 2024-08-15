74eef50539f423216a1839632d6917aee20059a3
KowshikSelvakumar <149224271+KowshikSelvakumar@users.noreply.github.com>
2024-08-15 07:41:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+//   }
+  
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  var menu = document.querySelector('.menu-links');
+  var menuIcon = document.querySelector('.menu-icon');
+  var cancelIcon = document.querySelector('.cancel-icon');
+
+  menu.classList.toggle('open');
+  menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none';
+  cancelIcon.style.display = cancelIcon.style.display === 'block' ? 'none' : 'block';
+}
+
+  // document.getElementById('hamburger').addEventListener('click', function() {
+  //   var navLinks = document.getElementById('nav-links-mobile');
+  //   if (navLinks.style.display === 'flex') {
+  //     navLinks.style.display = 'none';
+  //   } else {
+  //     navLinks.style.display = 'flex';
+  //     navLinks.style.flexDirection = 'column';
+  //   }
+  // });
   
